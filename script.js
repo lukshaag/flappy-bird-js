1b44cb0554c81931b07a97f4a1b4babc4b3f1ab3
lukshaag <lucashaagsaraiva@gmail.com>
2022-01-31 16:18:21 -0300

    game base

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log('Flappy bird Js');
+
+const sprites = new Image();
+sprites.src = '.img/sprites.png';
+
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
